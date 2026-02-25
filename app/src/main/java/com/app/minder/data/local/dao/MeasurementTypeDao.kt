bdf27b040133c1990b9a183b519c3f9317f33d13
KosichkaCheese <selivanovaalina9@gmail.com>
2026-02-25 18:27:27 +0500

    added MeasurementTypeDao

---
+++ app/src/main/java/com/app/minder/data/local/dao/MeasurementTypeDao.kt
@@ -0,0 +1,21 @@
+package com.app.minder.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.app.minder.data.local.entity.MeasurementEntity
+import com.app.minder.data.local.entity.MeasurementTypeEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface MeasurementTypeDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertAll(types: List<MeasurementTypeEntity>)
+
+    @Query("SELECT COUNT(*) FROM measurement_types")
+    suspend fun getCount(): Int
+
+    @Query("SELECT * FROM measurement_types")
+    fun getAllMeasurementTypes(): Flow<List<MeasurementTypeEntity>>
+}
