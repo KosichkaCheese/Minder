c8903265a4be36ca5093d258a7950ebbab78b037
KosichkaCheese <selivanovaalina9@gmail.com>
2026-04-27 19:55:10 +0500

    fix: padding for keyboard

--- app/src/main/java/com/app/minder/presentation/medDetail/MedDetailScreen.kt
+++ app/src/main/java/com/app/minder/presentation/medDetail/MedDetailScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -158,6 +159,7 @@ fun MedDetailScreen(
     Column(
         modifier = Modifier
             .fillMaxSize()
+            .imePadding()
             .padding(start = 16.dp, bottom = 0.dp, end = 16.dp, top = 0.dp)
             .verticalScroll(rememberScrollState()),
         horizontalAlignment = Alignment.CenterHorizontally,
